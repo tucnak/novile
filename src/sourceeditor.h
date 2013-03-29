ddbc25adab312f9bc20ba775d99cc16a0cfd1838
Illya Kovalevskyy <illya.kovalevskyy@gmail.com>
2013-03-29 14:29:29 +0200

    License comments were added

--- src/sourceeditor.h
+++ src/sourceeditor.h
@@ -1,3 +1,13 @@
+/*
+ * This file is part of the Novile Editor
+ * This program is free software licensed under the GNU LGPL. You can
+ * find a copy of this license in LICENSE in the top directory of
+ * the source code.
+ *
+ * Copyright 2013    Illya Kovalevskyy   <illya.kovalevskyy@gmail.com>
+ *
+ */
+
 #ifndef SOURCEEDITOR_H
 #define SOURCEEDITOR_H
 
